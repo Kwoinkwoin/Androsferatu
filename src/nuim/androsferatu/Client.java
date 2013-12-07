7ace2e71d672f403f1cc8a06de8bcfdd403f2cd9
Canard <Canard@PC-Canard>
2013-12-07 16:33:28 +0000

    backup2

--- src/nuim/androsferatu/Client.java
+++ src/nuim/androsferatu/Client.java
@@ -6,7 +6,7 @@ public class Client {
 
 	private Socket socket;
 	private GameActivity gameActivity;
-	private Player player;
+	private Player players;
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
