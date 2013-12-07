c54768e493d1eda6fe701fa6a7b79ae08d49721b
Canard <Canard@PC-Canard>
2013-12-07 17:39:10 +0000

    intro

--- gen/nuim/androsferatu/R.java
+++ gen/nuim/androsferatu/R.java
@@ -22,23 +22,44 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int nosferatu_ambiance4=0x7f020001;
+        public static final int nosferatu_boite=0x7f020002;
+        public static final int nosferatu_composants=0x7f020003;
+        public static final int nosferatu_illo1=0x7f020004;
+        public static final int nosferatu_illo2=0x7f020005;
+        public static final int nosferatu_illo3=0x7f020006;
+        public static final int nosferatu_illo4=0x7f020007;
+        public static final int nosferatu_nuit=0x7f020008;
+        public static final int nosferatu_titre=0x7f020009;
+        public static final int nosferatu_view1=0x7f02000a;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080003;
+        public static final int button1=0x7f080002;
+        public static final int editText1=0x7f080000;
+        public static final int textView1=0x7f080001;
     }
     public static final class layout {
-        public static final int activity_game=0x7f030000;
-        public static final int activity_main=0x7f030001;
+        public static final int activity_activity_log_in=0x7f030000;
+        public static final int activity_game=0x7f030001;
+        public static final int activity_main=0x7f030002;
     }
     public static final class menu {
-        public static final int game=0x7f070000;
-        public static final int main=0x7f070001;
+        public static final int activity_log_in=0x7f070000;
+        public static final int game=0x7f070001;
+        public static final int main=0x7f070002;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int exit=0x7f050006;
+        public static final int hello_world=0x7f050009;
+        public static final int login=0x7f050005;
+        public static final int ok=0x7f050008;
+        public static final int start=0x7f050002;
+        public static final int title_activity_activity_log_in=0x7f050007;
         public static final int title_activity_game=0x7f050003;
+        public static final int username=0x7f050004;
     }
     public static final class style {
         /** 
--- src/nuim/androsferatu/MainActivity.java
+++ src/nuim/androsferatu/MainActivity.java
@@ -1,6 +1,7 @@
 package nuim.androsferatu;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
@@ -21,7 +22,8 @@ public class MainActivity extends Activity {
 	}
 	
 	public void start(View view) {
-		
+		Intent intent = new Intent(this, ActivityLogIn.class);
+		startActivity(intent);
 	}
 
 }
