016237fa336b17c0a36b8eef8eb1164c72b83d6c
Pierrick Lauffenburger <cubitus@Cubitus-Trotter.home>
2013-12-07 17:23:25 +0100

    Android chat client

--- src/nuim/androsferatu/MainActivity.java
+++ src/nuim/androsferatu/MainActivity.java
@@ -1,9 +1,8 @@
 package nuim.androsferatu;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.app.Activity;
 import android.view.Menu;
-import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -18,10 +17,6 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
-	}
-	
-	public void start(View view) {
-		
 	}
 
 }
